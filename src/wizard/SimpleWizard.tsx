ae55bc052662dd4a96574fc22638f30d43865e68
Charles Jacquin <charles.jacquin@autistici.org>
2017-12-09 13:44:02 +0100

    style(types): add correct type to Simplewizard

--- src/wizard/SimpleWizard.tsx
+++ src/wizard/SimpleWizard.tsx
@@ -1,6 +1,8 @@
+import { WizardProps } from '../types'
+
 /**
  * Helper wich render a dead simple wizard
  * @param param - the props from wizard HoC
  * @return {JSX.Element} the currentForm of the wizard
  */
-export const SimpleWizard = ({ renderCurrentForm }) => renderCurrentForm()
+export const SimpleWizard = ({ renderCurrentForm }: WizardProps) => renderCurrentForm()
