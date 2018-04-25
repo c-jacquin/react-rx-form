da394868ccbcb08f65b137e79bb78acdec6c64fd
Charles Jacquin <charles.jacquin@autistici.org>
2018-04-25 22:27:19 +0200

    style(prettier): add prettier config for tslint

--- src/observable/factory.ts
+++ src/observable/factory.ts
@@ -1,19 +1,6 @@
 import { fromEvent } from 'rxjs/observable/fromEvent'
 import { merge } from 'rxjs/observable/merge'
 
-// import 'rxjs/add/observable/merge'
-// import 'rxjs/add/observable/fromEvent'
-// import 'rxjs/add/observable/of'
-
-// import 'rxjs/add/operator/do'
-// import 'rxjs/add/operator/filter'
-// import 'rxjs/add/operator/map'
-// import 'rxjs/add/operator/merge'
-// import 'rxjs/add/operator/mergeMap'
-// import 'rxjs/add/operator/switchMap'
-// import 'rxjs/add/operator/debounceTime'
-// import 'rxjs/add/operator/throttleTime'
-
 import { ObsFactory, InputObsParams, SelectObsParams, FormObsParams } from '../types'
 
 /**
--- styleguide.config.js
+++ styleguide.config.js
@@ -3,28 +3,27 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    title: 'react-rx-form',
-    styleguideDir: 'docs',
-    components: 'src/**/*.tsx',
-    highlightTheme: 'material',    
-    resolver: require('react-docgen').resolver.findAllComponentDefinitions,
-    propsParser: require('react-docgen-typescript').withDefaultConfig().parse,
-    webpackConfig: {
-        entry: {
-            app: path.resolve(__dirname, 'src/index.ts')
-        },
-        resolve: {
-            extensions: [".ts", ".tsx"],
-            modules: ['node_modules']
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    exclude: /^.\/src\/.*\/__tests__\/.*$/,
-                    loader: 'awesome-typescript-loader'
-                }
-            ]
+  title: 'react-rx-form',
+  styleguideDir: 'docs',
+  components: 'src/**/*.tsx',
+  resolver: require('react-docgen').resolver.findAllComponentDefinitions,
+  propsParser: require('react-docgen-typescript').withDefaultConfig().parse,
+  webpackConfig: {
+    entry: {
+      app: path.resolve(__dirname, 'src/index.ts')
+    },
+    resolve: {
+      extensions: [".ts", ".tsx"],
+      modules: ['node_modules']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /^.\/src\/.*\/__tests__\/.*$/,
+          loader: 'awesome-typescript-loader'
         }
-    }    
+      ]
+    }
+  }
 }
