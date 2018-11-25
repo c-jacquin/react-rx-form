2275f945cc016417bcae08e7b0bef8c4d05082be
Charles Jacquin <charles.jacquin@autistici.org>
2018-11-25 23:13:04 +0100

    fix(typings): add back ts typings

--- index.d.ts
+++ index.d.ts
@@ -15,9 +15,9 @@ import {
     WizardParams,
     WizardProps,
     WizardState,
-} from '../src/types'
+} from './src/types'
 
-export * from '../src/types'
+export * from './src/types'
 export declare function rxForm<Props>(params: RxFormParams<Props>): (Comp: React.ComponentClass<Props & RxFormProps>) => React.ComponentClass<Props & RequiredProps>
 export declare function wizard<Props>(params: WizardParams<Props>): (Comp: React.ComponentClass<Props & WizardProps>) => React.ComponentClass<Props & RequiredProps>
 export declare function SimpleWizard(props: WizardProps): JSX.Element
